c0e62348878a85901b0163f35512306bde2106f1
Bob7650 <drazekhubert125@gmail.com>
2025-05-21 21:05:33 +0200

    Added bluetooth setup and primitive permission request

--- app/src/main/java/drazek/jiyt/MainActivity.kt
+++ app/src/main/java/drazek/jiyt/MainActivity.kt
@@ -1,30 +1,41 @@
 package drazek.jiyt
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import drazek.jiyt.ui.components.JiytModalDrawerSheet
 import drazek.jiyt.ui.components.JiytTopAppBar
 import drazek.jiyt.ui.theme.JiytTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+
+    var resultPermissionRequestLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ accepted ->
+        println(accepted)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -52,8 +63,55 @@ class MainActivity : ComponentActivity() {
                             })
                         }
                     ) { contentPadding ->
+                        Button(
+                            onClick = {
+                                if(ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_CONNECT)==PERMISSION_GRANTED)
+                                    setupBluetooth()
+                                else
+                                    resultPermissionRequestLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+                            },
+                            modifier = Modifier.padding(contentPadding)
+                        ) {
+                            Text("Send Data")
+                        }
                     }
                 }
+            }
+        }
+    }
+
+    var resultBluetoothLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if(result.resultCode == RESULT_OK){
+            //bluetooth available
+            Toast.makeText(this,"Bluetooth enabled",Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this,"Canceled",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+    private fun setupBluetooth(){
+        val bluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+
+        if(bluetoothAdapter == null){
+            //no bluetooth support TODO: inform user what happened
+            return
+        }
+
+        if(!bluetoothAdapter.isEnabled){
+            resultBluetoothLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+        }
+    }
+
+    @Composable
+    private fun PrevButton() {
+        JiytTheme {
+            Button(
+                onClick = {},
+                modifier = Modifier.padding()
+            ) {
+                Text("Send Data")
             }
         }
     }
