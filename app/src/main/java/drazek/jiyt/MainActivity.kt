1acae202b967b0ecdc224271f8e819e48aeb5420
Bob7650 <drazekhubert125@gmail.com>
2025-05-25 01:25:33 +0200

    Working bluetooth with bugs and simple UI

--- app/src/main/java/drazek/jiyt/MainActivity.kt
+++ app/src/main/java/drazek/jiyt/MainActivity.kt
@@ -1,146 +1,226 @@
 package drazek.jiyt
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.companion.CompanionDeviceManager
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
+import androidx.activity.viewModels
+import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
-import drazek.jiyt.ui.components.JiytModalDrawerSheet
-import drazek.jiyt.ui.components.JiytTopAppBar
+import drazek.jiyt.ui.AnimListScreen
 import drazek.jiyt.ui.theme.JiytTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
+    private val animViewModel: JiytViewModelAnimList by viewModels()
     private val permissions = arrayOf(
         Manifest.permission.BLUETOOTH_SCAN,
         Manifest.permission.BLUETOOTH_CONNECT
     )
 
-    private var enableBluetoothLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-        if(result.resultCode == RESULT_OK){
-            //bluetooth enabled
-            Toast.makeText(this, "Enabled bluetooth for you :*", Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(this,"Unable to activate bluetooth",Toast.LENGTH_LONG).show()
-        }
-    }
 
-    private var askForPermissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ granted ->
-        for(grant in granted){
-            if(grant.value){
-                Toast.makeText(this,"${grant.key} granted", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this,"Couldn't grant ${grant.key}", Toast.LENGTH_LONG).show()
+    //** LAUNCHERS **//
+    private var enableBluetoothLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                //bluetooth enabled
+                //making sure that there is no broken connection
+                animViewModel.terminateConnection()
+                Toast.makeText(this, "Enabled bluetooth for you :*", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Unable to activate bluetooth", Toast.LENGTH_LONG).show()
             }
         }
-    }
+
+    @SuppressLint("MissingPermission")
+    private var askForPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { granted ->
+            val allGranted = granted.all {
+                it.value
+            }
+
+            if (allGranted) {
+                Toast.makeText(this, "${granted.keys} granted", Toast.LENGTH_SHORT).show()
+                animViewModel.setPermissionsGranted(true)
+                animViewModel.setBondedDevices()
+            } else {
+                Toast.makeText(this, "Couldn't grant ${granted.keys}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    @SuppressLint("MissingPermission")
+    private var deviceChooserLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val device =
+                    result.data?.getParcelableExtra<BluetoothDevice>(CompanionDeviceManager.EXTRA_DEVICE)
+
+                // Permissions already checked, called from function that requires them
+                Log.println(Log.INFO, "Bluetooth", "Connecting to ${device!!.name}")
+                animViewModel.connectToDevice(device,this)
+            }
+        }
+    //***************//
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         val bluetoothManager = application.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
-        val bluetoothAdapter = bluetoothManager.adapter
+        if (bluetoothManager.adapter == null) {
+            //bluetooth not supported on this device
+        } else {
+            animViewModel.storeBluetoothAdapter(bluetoothManager.adapter)
+        }
 
-        val factory = JiytAnimListViewModel.Companion.JiytAnimListViewModelFactory(application, bluetoothAdapter)
-        val animViewModel: JiytAnimListViewModel = ViewModelProvider(this, factory)[JiytAnimListViewModel::class.java]
+        askForPermissionsLauncher.launch(permissions)
 
         setContent {
-            MainScreenView(animViewModel, bluetoothAdapter)
+            MainScreenView()
         }
     }
 
     @Composable
-    fun MainScreenView(viewModel: JiytAnimListViewModel, bluetoothAdapter: BluetoothAdapter) {
-
-        val permissionGranted = remember {
-                permissions.all {
-                    ContextCompat.checkSelfPermission(
-                        this,
-                        it
-                    ) == PackageManager.PERMISSION_GRANTED
-                }
-        }
-        viewModel.setPermissionsGranted(permissionGranted)
-
-        val granted by viewModel.permissionsGranted.collectAsState()
-
+    fun MainScreenView() {
         JiytTheme {
-            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-            val scope = rememberCoroutineScope()
-            ModalNavigationDrawer(
-                drawerState = drawerState,
-                drawerContent = {
-                    JiytModalDrawerSheet()
-                },
-            ) {
-                Scaffold(
-                    topBar = {
-                        JiytTopAppBar(onClick = {
-                            if (it == "menu"){
-                                scope.launch {
-                                    drawerState.apply {
-                                        if (isClosed) open() else close()
-                                    }
-                                }
-                            }
-                        })
-                    }
-                ) { contentPadding ->
-                    Button(
-                        onClick = {
-                            when {
-                                !bluetoothAdapter.isEnabled -> {
-                                    enableBluetoothLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
-                                }
-                                !granted -> {
-                                    askForPermissionsLauncher.launch(permissions)
-                                }
-                                else -> {
-                                    viewModel.scanForDevices()
-                                }
-                            }
-                        },
-                        modifier = Modifier.padding(contentPadding)
-                    ) {
-                        Text("Start Scanning")
-                    }
-                }
-            }
+            AnimListScreen(
+                associationOnClick = { associateOnClick() },
+                logOnClick = { logOnClick() },
+                sendMessageOnClick = { sendMessageOnClick() },
+                selectDevice = { name -> selectDevice(name) },
+                refreshOnClick = { refreshOnClick() },
+                animViewModel
+            )
         }
     }
 
-    @Composable
-    private fun PrevButton() {
-        JiytTheme {
-            Button(
-                onClick = {},
-                modifier = Modifier.padding()
-            ) {
-                Text("Send Data")
+    @SuppressLint("MissingPermission")
+    private fun refreshOnClick() {
+        if (checkAndAskForPermissions()) {
+            animViewModel.setBondedDevices()
+        }
+    }
+
+    private fun sendMessageOnClick() {
+        if (animViewModel.sendMessageData()) {
+            Toast.makeText(this, "Data sent: \"Hello :D\"", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Unable to send data", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun logOnClick() {
+        if (checkAndAskForPermissions()) {
+            Toast.makeText(
+                this,
+                animViewModel.getConnectionName(),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun associateOnClick() {
+        if (checkAndAskForPermissions()) {
+            makeAssociation()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun selectDevice(name: String) {
+        if (checkAndAskForPermissions()) {
+            animViewModel.bondedDevices.value!!.forEach {
+                if (it.name == name) {
+                    animViewModel.connectToDevice(it, this)
+                    Toast.makeText(this,"Connecting...", Toast.LENGTH_SHORT).show()
+                    return
+                }
             }
         }
+        Toast.makeText(this, "No device $name", Toast.LENGTH_LONG).show()
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+    private fun makeAssociation() {
+        // create association request (what devices you can see)
+        animViewModel.createAssociationRequest()
+
+        // creating an async popup that launches a deviceChooserLauncher
+        // when user makes a choice or timeout
+        val manager = getSystemService(CompanionDeviceManager::class.java)
+
+        manager.associate(
+            animViewModel.associationRequest.value!!,
+            object : CompanionDeviceManager.Callback() {
+                override fun onAssociationPending(intentSender: IntentSender) {
+                    deviceChooserLauncher.launch(IntentSenderRequest.Builder(intentSender).build())
+                }
+
+                override fun onFailure(error: CharSequence?) {
+                    Log.e("CDM", "Association failed: $error")
+                }
+            },
+            null
+        )
+    }
+
+    private fun checkAndAskForPermissions(): Boolean {
+        when {
+            animViewModel.bluetoothAdapter.value == null -> {
+                // bluetooth not supported
+                Toast.makeText(
+                    this,
+                    "Bluetooth is not supported on this device D:",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            !animViewModel.bluetoothAdapter.value!!.isEnabled -> {
+                // bluetooth is off
+                Toast.makeText(this, "Bluetooth is off", Toast.LENGTH_LONG).show()
+            }
+
+            animViewModel.permissionsGranted.value == null || !animViewModel.permissionsGranted.value!! -> {
+                askForPermissionsLauncher.launch(permissions)
+            }
+
+            else -> {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun checkAndTurnOnBluetooth(animViewModel: JiytViewModelAnimList): Boolean {
+        when {
+            !animViewModel.bluetoothAdapter.value!!.isEnabled -> {
+                enableBluetoothLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+            }
+
+            else -> {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        animViewModel.terminateConnection()
     }
 }
--- app/src/main/java/drazek/jiyt/ui/components/JiytTopAppBar.kt
+++ app/src/main/java/drazek/jiyt/ui/components/JiytTopAppBar.kt
@@ -1,8 +1,6 @@
 package drazek.jiyt.ui.components
 
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import drazek.jiyt.ui.theme.JiytTheme
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
